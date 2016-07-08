549e9569e9b085b7a357e852388d0b79345f7bc4
JaNe <j.nebil@gmail.com>
2016-07-08 19:18:03 +0200

    Fullstack App

--- collections/menu/drink.js
+++ collections/menu/drink.js
@@ -1,3 +1,27 @@
 DB_DrinkHappy = new Mongo.Collection('drinkHappy');
 
 DB_DrinkRegular = new Mongo.Collection('drinkRegular');
+
+DB_DrinkHappy.allow({
+  insert: function() {
+    return (Meteor.userId() === ''); //Admin Id between single quotes
+  },
+  update: function() {
+    return (Meteor.userId() === '');
+  },
+  remove: function() {
+    return (Meteor.userId() === '');
+  }
+});
+
+DB_Drinkegular.allow({
+  insert: function() {
+    return (Meteor.userId() === ''); //Admin Id between single quotes
+  },
+  update: function() {
+    return (Meteor.userId() === '');
+  },
+  remove: function() {
+    return (Meteor.userId() === '');
+  }
+});
