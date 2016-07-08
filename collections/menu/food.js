452b7dd3105ab8ffe4a7cb0555221361b0d7fe61
JaNe <j.nebil@gmail.com>
2016-07-08 19:18:53 +0200

    Fullstack App

--- collections/menu/food.js
+++ collections/menu/food.js
@@ -1,1 +1,13 @@
 DB_Food = new Mongo.Collection('food');
+
+DB_Food.allow({
+  insert: function() {
+    return (Meteor.userId() === ''); //Admin Id between single quotes
+  },
+  update: function() {
+    return (Meteor.userId() === '');
+  },
+  remove: function() {
+    return (Meteor.userId() === '');
+  }
+});
