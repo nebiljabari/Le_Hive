9b0c8e71fea92d6f571b43f0591513c9163db73c
JaNe <j.nebil@gmail.com>
2016-07-08 19:22:28 +0200

    Fullstack App

--- collections/todolist.js
+++ collections/todolist.js
@@ -1,1 +1,7 @@
 DB_Todolist = new Mongo.Collection('todos');
+
+DB_Todolist.allow({
+  insert: function() { return true; },
+  update: function() { return true; },
+  remove: function() { return true; }
+});
